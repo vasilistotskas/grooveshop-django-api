f49184a2f3b4c14d44b7ebb2eab8669c29e0c37d
Gro0ve <vassilistotskas@msn.com>
2023-07-23 15:10:44 +0300

    fix(pyproject.toml, setup.py): Update semantic release configuration

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import sys
+
 from setuptools import setup
 
 __version__ = "0.8.8"
@@ -9,3 +11,9 @@ setup(
     author="Vasilis Totskas",
     author_email="vassilistotskas@msn.com",
 )
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass

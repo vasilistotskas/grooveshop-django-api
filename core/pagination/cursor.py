441078eea389a82ce50183ccfe077cb397722122
Gro0ve <vassilistotskas@msn.com>
2023-08-24 11:46:29 +0300

    feat(pagination): Cursor pagination fixed and tests implemented.

--- core/pagination/cursor.py
+++ core/pagination/cursor.py
@@ -6,16 +6,18 @@ from rest_framework.response import Response
 
 class CursorPaginator(pagination.CursorPagination):
     page_size = 100
+    page_size_query_param = "page_size"
     cursor_query_param = "c"
     total_items = 0
     total_pages = 0
-    current_page_number = 0
+    ordering = "-created_at"
+    has_next = False
+    has_previous = False
 
     def paginate_queryset(self, queryset, request, view=None):
         self.total_items = queryset.count()
         self.total_pages = self.get_total_pages()
         super().paginate_queryset(queryset, request, view)
-        self.current_page_number = self.get_current_page_number(request)
         return self.page
 
     def get_total_pages(self) -> int:
@@ -23,16 +25,6 @@ class CursorPaginator(pagination.CursorPagination):
         if self.total_items % self.page_size != 0:
             total_pages += 1
         return total_pages
-
-    # @TODO: This is not working properly.
-    def get_current_page_number(self, request) -> int:
-        cursor = self.decode_cursor(request)
-        if cursor is None:
-            return 1
-        else:
-            cursor_offset = cursor[0]
-            current_page = cursor_offset / self.page_size + 1
-            return current_page
 
     def get_paginated_response(self, data) -> Response:
         return Response(
@@ -45,7 +37,6 @@ class CursorPaginator(pagination.CursorPagination):
                 "total_pages": self.total_pages,
                 "page_size": self.page_size,
                 "page_total_results": len(data),
-                "page": self.current_page_number,
                 "results": data,
             }
         )
---
+++ tests/unit/core/pagination/test_cursor.py
@@ -0,0 +1,56 @@
+from django.test import RequestFactory
+from django.test import TestCase
+from rest_framework.request import Request
+
+from core.pagination.cursor import CursorPaginator
+from product.models.product import Product
+
+
+class CursorPaginatorTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        # Create some test data
+        for i in range(1, 101):
+            Product.objects.create(
+                slug=f"product-{i}",
+                price=10.00,
+                active=True,
+                stock=10,
+                discount_percent=5.00,
+                hits=0,
+                weight=0.00,
+            )
+
+    def test_paginate_queryset(self):
+        paginator = CursorPaginator()
+        queryset = Product.objects.all()
+
+        # Create a Request instance from Django REST framework
+        request = Request(self.factory.get("/api/v1/products/?c=MA==&page_size=10"))
+
+        paginated_queryset = paginator.paginate_queryset(queryset, request)
+
+        self.assertEqual(len(paginated_queryset), 10)
+
+    def test_get_total_pages(self):
+        paginator = CursorPaginator()
+        paginator.total_items = 105
+        total_pages = paginator.get_total_pages()
+
+        self.assertEqual(total_pages, 3)  # 105 items should be split into 3 pages
+
+    def test_get_paginated_response(self):
+        paginator = CursorPaginator()
+        data = [{"name": f"Item {i}"} for i in range(1, 11)]
+        response = paginator.get_paginated_response(data)
+
+        self.assertIn("links", response.data)
+        self.assertIn("count", response.data)
+        self.assertIn("total_pages", response.data)
+        self.assertIn("page_size", response.data)
+        self.assertIn("page_total_results", response.data)
+        self.assertIn("results", response.data)
+        self.assertEqual(response.data["page_total_results"], len(data))
+
+    def tearDown(self):
+        Product.objects.all().delete()

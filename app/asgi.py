9090c28ec3babf9a60cd3845b3364d6d16e3fc45
Gro0ve <vassilistotskas@msn.com>
2023-10-31 22:04:56 +0200

    feat: Versions bump, `asgi.py` router improved.

--- app/asgi.py
+++ app/asgi.py
@@ -1,7 +1,9 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from channels.routing import URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
@@ -11,5 +13,10 @@ django_application = get_asgi_application()
 from . import urls  # noqa isort:skip
 
 application = ProtocolTypeRouter(
-    {"http": get_asgi_application(), "websocket": URLRouter(urls.websocket_urlpatterns)}
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(urls.websocket_urlpatterns))
+        ),
+    }
 )

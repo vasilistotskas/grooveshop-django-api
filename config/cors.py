cde1bccc15ad5e814c02ac7f1f42a54629e8be85
Gro0ve <vassilistotskas@msn.com>
2023-11-04 01:21:38 +0200

    feat(config): Added `NGINX_BASE_URL` .

--- config/cors.py
+++ config/cors.py
@@ -1,12 +1,14 @@
 from os import getenv
 
 APP_BASE_URL = getenv("APP_BASE_URL", "http://localhost:8000")
+NGINX_BASE_URL = getenv("NGINX_BASE_URL", "http://localhost:8010")
 NUXT_BASE_URL = getenv("NUXT_BASE_URL", "http://localhost:3000")
 MEDIA_STREAM_BASE_URL = getenv("MEDIA_STREAM_BASE_URL", "http://localhost:3003")
 
 CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
 CORS_ALLOWED_ORIGINS = [
     APP_BASE_URL,
+    NGINX_BASE_URL,
     NUXT_BASE_URL,
     MEDIA_STREAM_BASE_URL,
     "http://localhost:1337",
@@ -15,6 +17,7 @@ CORS_ORIGIN_ALLOW_ALL = getenv("CORS_ORIGIN_ALLOW_ALL", "True") == "True"
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     APP_BASE_URL,
+    NGINX_BASE_URL,
     NUXT_BASE_URL,
     MEDIA_STREAM_BASE_URL,
     "http://localhost:1337",
--- config/csrf.py
+++ config/csrf.py
@@ -3,6 +3,7 @@ from os import getenv
 DEBUG = getenv("DEBUG", "True") == "True"
 
 APP_BASE_URL = getenv("APP_BASE_URL", "http://localhost:8000")
+NGINX_BASE_URL = getenv("NGINX_BASE_URL", "http://localhost:8010")
 NUXT_BASE_URL = getenv("NUXT_BASE_URL", "http://localhost:3000")
 MEDIA_STREAM_BASE_URL = getenv("MEDIA_STREAM_BASE_URL", "http://localhost:3003")
 
@@ -16,6 +17,7 @@ CSRF_COOKIE_SAMESITE = "Lax"
 CSRF_HEADER_NAME = "HTTP_X_CSRFTOKEN"
 CSRF_TRUSTED_ORIGINS = [
     APP_BASE_URL,
+    NGINX_BASE_URL,
     NUXT_BASE_URL,
     MEDIA_STREAM_BASE_URL,
     "http://localhost:1337",

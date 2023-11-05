21d798eab75db750906debf59a6b099ed4f29e39
Gro0ve <vassilistotskas@msn.com>
2023-11-05 11:48:30 +0200

    fix: session middleware `update_session` wrap with try catch and versions bump.

--- session/middleware.py
+++ session/middleware.py
@@ -27,41 +27,50 @@ class SessionTraceMiddleware:
         return response
 
     def update_session(self, request):
-        user_cache_key = generate_user_cache_key(request)
+        try:
+            user_cache_key = generate_user_cache_key(request)
 
-        now = timezone.now()
-        now_str = now.isoformat()
-        user = request.session.get("user", None)
-        http_referer = request.META.get("HTTP_REFERER", None)
-        user_agent = request.META.get("HTTP_USER_AGENT", None)
-        session_key = request.session.session_key
-        cart_id = request.session.get("cart_id", None)
+            now = timezone.now()
+            now_str = now.isoformat()
+            user = request.session.get("user", None)
+            http_referer = request.META.get("HTTP_REFERER", None)
+            user_agent = request.META.get("HTTP_USER_AGENT", None)
+            session_key = request.session.session_key
+            cart_id = request.session.get("cart_id", None)
 
-        request.session["last_activity"] = now_str
-        request.META["HTTP_REFERER"] = http_referer
-        request.META["HTTP_USER_AGENT"] = user_agent
-        request.session["session_key"] = session_key
-        request.session["cart_id"] = cart_id
-        request.session.modified = True
-        request.session.save()
+            request.session["last_activity"] = now_str
+            request.META["HTTP_REFERER"] = http_referer
+            request.META["HTTP_USER_AGENT"] = user_agent
+            request.session["session_key"] = session_key
+            request.session["cart_id"] = cart_id
+            request.session.modified = True
+            request.session.save()
 
-        if user is not None:
-            json_user = json.dumps(
-                {"id": request.user.id, "email": request.user.email},
-                cls=DjangoJSONEncoder,
+            if user is not None:
+                json_user = json.dumps(
+                    {"id": request.user.id, "email": request.user.email},
+                    cls=DjangoJSONEncoder,
+                )
+            else:
+                json_user = None
+
+            cache_data = {
+                "last_activity": now_str,
+                "user": json_user,
+                "referer": http_referer,
+                "user_agent": user_agent,
+                "session_key": session_key,
+                "cart_id": cart_id,
+            }
+            cache_instance.set(user_cache_key, cache_data, caches.ONE_HOUR)
+        except Exception as e:
+            logger.error(
+                "SessionTraceMiddleware failed to update session",
+                extra={
+                    "request": request,
+                    "exception": e,
+                },
             )
-        else:
-            json_user = None
-
-        cache_data = {
-            "last_activity": now_str,
-            "user": json_user,
-            "referer": http_referer,
-            "user_agent": user_agent,
-            "session_key": session_key,
-            "cart_id": cart_id,
-        }
-        cache_instance.set(user_cache_key, cache_data, caches.ONE_HOUR)
 
     def log_request(self, request, response):
         logger.info(

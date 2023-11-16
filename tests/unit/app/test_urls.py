7ffd294d8002182e8f83ca3fe9e97e26c1d3c95d
Gro0ve <vassilistotskas@msn.com>
2023-11-16 21:26:02 +0200

    fix: remove useless test

--- tests/unit/app/test_urls.py
+++ tests/unit/app/test_urls.py
@@ -9,11 +9,3 @@ class DebugToolbarURLsTestCase(TestCase):
         # When DEBUG is False, the debug toolbar URL should not work
         response = self.client.get("/__debug__/", follow=True)
         self.assertEqual(response.status_code, 404)
-
-    def test_static_url(self):
-        # Testing a known static file URL
-        response = self.client.get(
-            f"{settings.STATIC_URL}images/no_photo.jpg", follow=True
-        )
-        # In DEBUG mode, static files should be served so the response shouldn't be 404
-        self.assertNotEqual(response.status_code, 404)

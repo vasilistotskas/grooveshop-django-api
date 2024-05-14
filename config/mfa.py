620905bb8c49cf570ddfa037bdb361f7538be259
vasilistotskas <vassilistotskas@msn.com>
2024-05-14 23:35:50 +0300

    feat: Add MFA_TOTP_ISSUER setting

--- config/mfa.py
+++ config/mfa.py
@@ -1,4 +1,7 @@
+from os import getenv
+
 MFA_ADAPTER = "allauth.mfa.adapter.DefaultMFAAdapter"
 MFA_RECOVERY_CODE_COUNT = 1
 MFA_TOTP_PERIOD = 60
 MFA_TOTP_DIGITS = 6
+MFA_TOTP_ISSUER = getenv("MFA_TOTP_ISSUER", "localhost")

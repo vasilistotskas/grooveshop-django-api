aa2cb76052af033b25671de510963ac9e733b38d
vasilistotskas <vassilistotskas@msn.com>
2024-08-22 01:53:54 +0300

    fix: liker_user info at `notify_comment_liked`

--- blog/signals.py
+++ blog/signals.py
@@ -1,5 +1,6 @@
 from asgiref.sync import sync_to_async
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
@@ -11,6 +12,8 @@ from notification.models.user import NotificationUser
 
 languages = [lang["code"] for lang in settings.PARLER_LANGUAGES[settings.SITE_ID]]
 
+User = get_user_model()
+
 
 @receiver(m2m_changed, sender=BlogComment.likes.through)
 async def notify_comment_liked(sender, instance, action, reverse, pk_set, **kwargs):
@@ -21,6 +24,7 @@ async def notify_comment_liked(sender, instance, action, reverse, pk_set, **kwar
 
         for user_id in pk_set:
             if user and user.id != user_id:
+                liker_user = await User.objects.aget(id=user_id)
                 notification = await Notification.objects.acreate(
                     kind=NotificationKindEnum.INFO,
                     link=blog_post_url,
@@ -35,7 +39,7 @@ async def notify_comment_liked(sender, instance, action, reverse, pk_set, **kwar
                         await sync_to_async(setattr)(
                             notification,
                             "message",
-                            f"Your comment was liked by {user.username if user.username else user.email}.",
+                            f"Your comment was liked by {liker_user.username if liker_user.username else liker_user.email}.",
                         )
                     elif language == "el":
                         await sync_to_async(setattr)(
@@ -44,7 +48,7 @@ async def notify_comment_liked(sender, instance, action, reverse, pk_set, **kwar
                         await sync_to_async(setattr)(
                             notification,
                             "message",
-                            f"Το σχόλιο σου άρεσε στον χρήστη {user.username if user.username else user.email}.",
+                            f"Το σχόλιο σου άρεσε στον χρήστη {liker_user.username if liker_user.username else liker_user.email}.",
                         )
                     await notification.asave()
 

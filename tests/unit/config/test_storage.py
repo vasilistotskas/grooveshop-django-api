92943ef3934bc6eb0b6b88c8fae39514d8a13d70
vasilistotskas <vassilistotskas@msn.com>
2024-08-25 16:34:20 +0300

    fix: correct CSS link in `account/base.html` and test

--- tests/unit/config/test_storage.py
+++ tests/unit/config/test_storage.py
@@ -39,10 +39,9 @@ class TestStorage(unittest.TestCase):
 
         AWS_STORAGE_BUCKET_NAME = getenv("AWS_STORAGE_BUCKET_NAME")
         AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-        STATIC_LOCATION = "static"
         PUBLIC_MEDIA_LOCATION = "media"
 
-        self.assertEqual(STATIC_URL, f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/")
+        self.assertEqual(STATIC_URL, f"https://{AWS_S3_CUSTOM_DOMAIN}/")
         self.assertEqual(
             MEDIA_URL,
             f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/",

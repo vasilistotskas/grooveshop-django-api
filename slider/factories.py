80bcc12ba01dfdda5acfbb2ef5b8b0717d3a2e15
vasilistotskas <vassilistotskas@msn.com>
2024-10-22 13:14:22 +0300

    fix: Slider factory title max length

--- slider/factories.py
+++ slider/factories.py
@@ -30,7 +30,7 @@ class SliderTranslationFactory(factory.django.DjangoModelFactory):
     language_code = factory.Iterator(available_languages)
     name = factory.Faker("word")
     url = factory.Faker("url")
-    title = factory.LazyAttribute(lambda obj: factory.Faker("sentence").generate()[:40])
+    title = factory.LazyAttribute(lambda obj: factory.Faker("sentence")[:40])
     description = factory.Faker("paragraph")
     master = factory.SubFactory("slider.factories.SliderFactory")
 

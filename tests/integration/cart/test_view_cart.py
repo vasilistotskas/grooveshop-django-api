4d67a9ae9e0c7848303a85f5246ee1ac75290ca5
Vasilis Totskas <vassilistotskas@msn.com>
2025-03-15 23:09:56 +0200

    fix: tests

--- tests/integration/cart/test_view_cart.py
+++ tests/integration/cart/test_view_cart.py
@@ -94,7 +94,7 @@ class CartViewSetTest(APITestCase):
             self.detail_url, data=self.update_data, format="json"
         )
 
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_cart_as_anonymous_user(self):
         self.client.force_authenticate(user=None)
@@ -137,7 +137,7 @@ class CartViewSetTest(APITestCase):
 
         response = self.client.get(self.detail_url)
 
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_cart_with_invalid_data(self):
         invalid_update_data = {

4ab08a8d33b1ce0b27e2305b87a497c1d25f5944
Vasilis Totskas <vassilistotskas@msn.com>
2025-04-13 01:37:33 +0300

    fix: revert tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./core/templates/**/*.html'],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  attributify: false,
+  variants: {
+    typography: ['dark'],
+  },
+}

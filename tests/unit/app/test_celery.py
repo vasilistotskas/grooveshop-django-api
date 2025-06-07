8d40865308f4ae0258d5c8b12ff8fae8a8277d78
Vasilis Totskas <vassilistotskas@msn.com>
2025-06-07 16:01:20 +0300

    fix: remove useless tests

--- tests/unit/app/test_celery.py
+++ tests/unit/app/test_celery.py
@@ -22,61 +22,6 @@ class CeleryConfigTestCase(TestCase):
         self.assertEqual(app.conf.timezone, mock_celery_instance.conf.timezone)
         self.assertTrue(app.autodiscover_tasks.called)
 
-    @patch.dict(os.environ, {"DEBUG": "True"})
-    @patch("core.celery.create_celery_app")
-    def test_debug_task_with_debug_true(self, mock_create_celery_app):
-        celery.app = mock_create_celery_app.return_value
-        celery.debug_task = Mock()
-
-        celery.app = celery.create_celery_app()
-        celery.debug_task()
-
-        self.assertTrue(celery.debug_task.called)
-
-    @patch.dict(os.environ, {"DEBUG": "False"})
-    @patch("core.celery.create_celery_app")
-    def test_debug_task_with_debug_false(self, mock_create_celery_app):
-        celery.app = mock_create_celery_app.return_value
-        celery.debug_task = Mock()
-
-        celery.app = celery.create_celery_app()
-        celery.debug_task()
-
-        self.assertTrue(celery.debug_task.called)
-
-    @patch("core.celery.get_channel_layer")
-    @patch("os.getenv")
-    @patch.dict(os.environ, {"DEBUG": "True"})
-    def test_debug_task_notification_with_debug_true(
-        self, mock_getenv, mock_get_channel_layer
-    ):
-        mock_getenv.return_value = "True"
-        mock_channel_layer = mock_get_channel_layer.return_value
-        mock_channel_layer.group_send = AsyncMock()
-
-        celery.debug_task_notification()
-
-        mock_channel_layer.group_send.assert_called_once_with(
-            "notifications",
-            {
-                "type": "send_notification",
-                "user": 1,
-                "seen": False,
-                "link": "https://www.google.com",
-                "kind": "info",
-                "translations": [
-                    {
-                        "en": {
-                            "message": "This is a test notification",
-                        },
-                        "el": {
-                            "message": "Αυτή είναι μια δοκιμαστική ειδοποίηση",
-                        },
-                    },
-                ],
-            },
-        )
-
     def tearDown(self):
         celery.app = None
         celery.debug_task = None

4641ade0fb2fb81b60b399182c31109fa00108e0
Vasilis Totskas <vassilistotskas@msn.com>
2025-06-27 00:34:04 +0300

    fix: test_str_representation

--- tests/integration/product/category/test_model_product_category.py
+++ tests/integration/product/category/test_model_product_category.py
@@ -29,7 +29,7 @@ class CategoryModelTestCase(TestCase):
 
     def test_str_representation_no_parent(self):
         self.assertEqual(
-            str(self.category), self.category.safe_translation_getter("name")
+            str(self.category), self.category.safe_translation_getter("name") or ""
         )
 
     def test_get_ordering_queryset_with_parent(self):

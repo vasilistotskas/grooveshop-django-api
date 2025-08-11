1ed3b457cc98268ecd60e7930c5ec5c6930f11be
Vasilis Totskas <vassilistotskas@msn.com>
2025-08-12 00:27:30 +0300

    feat: Add full and short name getters to UserAccount
    Introduces get_full_name and get_short_name methods to the UserAccount model for easier access to user's full name and username.

--- user/models/account.py
+++ user/models/account.py
@@ -127,6 +127,12 @@ class UserAccount(
     def __str__(self):
         return self.username if self.username else self.email
 
+    def get_full_name(self):
+        return self.full_name
+
+    def get_short_name(self):
+        return self.username
+
     @property
     def active_subscriptions(self):
         return self.subscriptions.filter(
